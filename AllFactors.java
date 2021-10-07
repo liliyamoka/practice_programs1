8138f12b0ffdabacc641aec5031e222f53bf390f
liliyamoka <80568239+liliyamoka@users.noreply.github.com>
2021-10-07 18:12:57 +0530

    Add files via upload

---
+++ AllFactors.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class AllFactors {
+
+	public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+		
+		System.out.print("enter any number : ");
+		int n=sc.nextInt();
+		
+		for(int i=1;i<=n;i++)
+		{
+			if(n%i==0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+
+	}
+
+}
