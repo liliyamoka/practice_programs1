ddad1341f894e2781472e1e7df5165983d2bff0e
liliyamoka <80568239+liliyamoka@users.noreply.github.com>
2021-10-27 17:13:10 +0530

    Update GrossSalary.java

--- GrossSalary.java
+++ GrossSalary.java
@@ -1,4 +1,4 @@
-/*14. Write a C program to input basic salary of an employee and calculate its Gross     salary according to following:
+/*14. Write a java program to input basic salary of an employee and calculate its Gross     salary according to following:
     Basic Salary <= 10000 : HRA = 20%, DA = 80%
     Basic Salary <= 20000 : HRA = 25%, DA = 90%
     Basic Salary > 20000 : HRA = 30%, DA = 95%*/
